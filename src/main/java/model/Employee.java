06404a9438cadfe4df4480d0a3e54fa54928a3f7
Raluca Andrei <andrei_raluca93@yahoo.com>
2024-10-03 22:05:42 +0300

    Added more fields and new class (in model) named Function

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -2,6 +2,9 @@ package model;
 
 import jakarta.persistence.*;
 
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -10,6 +13,12 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
     @OneToOne
     private User user;
+    private String email;
+    private LocalDate startDay;
+    private Employee manager;
+    @OneToMany
+    private List<LeaveRequest> leaveRequestList;
 }
--- src/main/java/model/Function.java
+++ src/main/java/model/Function.java
@@ -2,19 +2,10 @@ package model;
 
 import jakarta.persistence.*;
 
-import java.util.List;
-
 @Entity
-@Table(name = "users")
-public class User {
-
+@Table(name = "functions")
+public class Function {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
-    private String password;
-
-
-
-
 }
