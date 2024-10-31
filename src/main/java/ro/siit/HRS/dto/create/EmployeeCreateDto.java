68b93daf593e19192767e8c70ed22aef890a8a25
Raluca Andrei <andrei_raluca93@yahoo.com>
2024-10-31 21:22:49 +0200

    field validations for employeeCreateDto

--- src/main/java/ro/siit/HRS/dto/create/EmployeeCreateDto.java
+++ src/main/java/ro/siit/HRS/dto/create/EmployeeCreateDto.java
@@ -1,22 +1,35 @@
 package ro.siit.HRS.dto.create;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Data
 public class EmployeeCreateDto {
 
+    @Pattern(regexp = "^[A-Z][a-z]+\\s[A-Z][a-z]+$", message = "Please enter a valid name!")
+    @Size(min = 4, max = 35, message = "Name should be at least 4 characters and maximum 35!")
     private String name;
+    @Email(message = "Please enter a valid email address!")
     private String email;
+    @NotEmpty(message = "City should not be empty")
     private String city;
+    @Pattern(regexp = "^\\d{10}$", message = "Number should contain 10 digits!")
     private String phoneNumber;
     private Long superiorId;
+    @NotEmpty(message = "Please enter the national Id!")
     private String nationalId;
+    @NotEmpty(message = "Please select the gender!")
     private String gender;
+    @NotEmpty(message = "Address should not be empty")
     private String address;
+    @FutureOrPresent(message = "Start date should be today or future, format yyyy-MM-dd!")
     private LocalDate startDate;
+    @Future(message = "End date should be in the future, format yyyy-MM-dd!")
     private LocalDate endDate;
+    @NotEmpty(message = "Please select the job title!")
     private String jobTitle;
 
 }
