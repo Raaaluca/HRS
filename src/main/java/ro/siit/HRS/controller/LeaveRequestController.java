c7021086b53f7260cf8362d2d83d1fe757e930ad
Raaaluca <andrei.raluca93@yahoo.com>
2026-05-02 08:37:55 +0300

    clean-code LeaveRequestController for REST design good practices

--- src/main/java/ro/siit/HRS/controller/LeaveRequestController.java
+++ src/main/java/ro/siit/HRS/controller/LeaveRequestController.java
@@ -1,21 +1,26 @@
 package ro.siit.HRS.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ro.siit.HRS.dto.create.LeaveRequestCreateDto;
 import ro.siit.HRS.dto.response.LeaveRequestReturnDto;
 import ro.siit.HRS.service.LeaveRequestService;
 
-@RequestMapping(path = "/leaverequest")
+@RequestMapping(path = "/leaverequests")
 @RestController
 @RequiredArgsConstructor
 public class LeaveRequestController {
 
     private final LeaveRequestService leaveRequestService;
 
-    @PostMapping(path = "/create")
-    public LeaveRequestReturnDto createLeaveRequest(@RequestBody LeaveRequestCreateDto leaveRequestCreateDto) {
+    @PostMapping
+    public ResponseEntity<LeaveRequestReturnDto> createLeaveRequest(@RequestBody @Valid LeaveRequestCreateDto leaveRequestCreateDto) {
 
-        return leaveRequestService.createLeaveRequest(leaveRequestCreateDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(leaveRequestService.createLeaveRequest(leaveRequestCreateDto));
     }
 }
